1b2121bcd69759bcdef5d9992250b72a269ef0ff
Pauleenaira <magistrado.airapauleen.m@gmail.com>
2026-03-14 20:59:16 +0800

    Initial commit

---
+++ JavaScript/ex.js
@@ -0,0 +1,17 @@
+function sumDeepStrictNumbers(arr) {
+  let sum = 0;
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      sum += sumDeepStrictNumbers(item);
+    } else if (typeof item === 'number' && !Number.isNaN(item)) {
+      sum += item;
+    }
+  }
+  return sum;
+}
+
+// Test Code
+const testArray1 = [10, ['5', [true, 5]], null, [undefined, [10, NaN]]];
+console.log(sumDeepStrictNumbers(testArray1)); // 25
+
+
